26ae11a2449e265c7ff619ea220054a1bf17e761
ensung2 <iieunsung@gmail.com>
2024-01-04 02:21:31 +0900

    qna 고양이 특성 작성완료, qna 디자인 및 프로그래스바 구현

--- js/view.js
+++ js/view.js
@@ -1,6 +1,39 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const endPoint = 10;
 
+function addAnswer(answerText, qIdx) {
+    var a = document.querySelector(".answerBox");
+    var answer = document.createElement("button");
+    answer.classList.add("answerList");
+    answer.classList.add("fadeIn");
+    a.appendChild(answer);
+    answer.innerHTML = answerText;
+
+    answer.addEventListener("click", function (){
+        var children = document.querySelectorAll(".answerList");
+        for (let i = 0; i<children.length; i++) {
+            children[i].disabled = true;
+            children[i].style.animation = "fadeOut 1s"
+        }
+        setTimeout(()=>{
+            for (let i = 0; i<children.length; i++) {
+                children[i].style.display="none";
+            }
+        goNext(++qIdx);
+        } ,950)
+    }, false);
+}
+
+function goNext(qIdx) {
+    var q = document.querySelector(".qBox");
+    q.innerHTML = qnaList[qIdx].q;
+    for(let i in qnaList[qIdx].a) {
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    }
+    var status = document.querySelector(".status_bar");
+    status.style.width = (100/endPoint) * (qIdx+1) + "%";
+}
 function begin() {
     main.style.animation = "fadeOut 1s";
     setTimeout(()=>{
@@ -9,5 +42,7 @@ function begin() {
             main.style.display = "none";
             qna.style.display = "block";
         },450)
+        let qIdx = 0;
+        goNext(qIdx);
     }, 450)
 }
