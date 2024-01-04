a3efcada332f7647441fe074e947ac81d704b002
ensung2 <iieunsung@gmail.com>
2024-01-04 23:57:12 +0900

    url 공유하기 버튼 1차 구현

--- js/view.js
+++ js/view.js
@@ -130,3 +130,13 @@ function begin() {
         goNext(qIdx);
     }, 450)
 }
+
+//현재 url 변수로 가져오기
+let nowUrl = decodeURI(window.location.protocol + '//' + window.location.hostname + window.location.pathname);
+
+function copyUrl(){
+//nowUrl 변수에 담긴 주소를
+    navigator.clipboard.writeText(nowUrl).then(res=>{
+        alert("주소가 복사되었습니다!");
+    })
+}
